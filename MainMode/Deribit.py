2d3bc41a0f884b9533175bb8c94e721b42e5654d
Kasymkhan <kasymkhankhubievnis@gmail.com>
2024-02-03 14:03:06 +0300

    added a new task

--- MainMode/Deribit.py
+++ MainMode/Deribit.py
@@ -1,7 +1,11 @@
 def get_coin_cost(coin: str) -> float:
     """
         returns a cost of a given coin from Deribit
+
+    
     """
+
+    # TODO make a function to get data from a Deribit API
 
     if coin == 'BTC':
         return 42600.0

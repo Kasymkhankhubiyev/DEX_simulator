2134cd657c3f9d744affdd1f56dffab8258490d6
Kasymkhan <kasymkhankhubievnis@gmail.com>
2024-02-03 14:02:22 +0300

    added a new task

--- MainMode/DEX.py
+++ MainMode/DEX.py
@@ -20,5 +20,25 @@ def get_pool_data(assetX: str, assetY: str) -> dict:
     # TODO: FOR Alexander
     # need to make a function that gets data trough API, packs in a specific format 
     # and returns data
-    
+
+    pass
+
+
+def get_historical_data(assetX: str, assetY: str, start_date = None, 
+                        end_date = None, time_step = '1h'):
+    """
+        This function returns a historical data of a pool state and trunsactions
+
+        Atributes:
+            assetX, str - a ticker name of an asset X
+            assetY, str - a ticker name of an asset Y
+            start_date, str - a date from which to start aggregating data, 
+                              if not provided, start from the first transaction in history
+            end_date, str - a date until which to agregate data,
+                            if not provided, get data until the current date
+            time_step, str - a time step, to agregate data, by default equals 1 hour
+
+        Returns:
+            data, dict (?)
+    """
     pass
